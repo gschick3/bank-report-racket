7c2f8693ce0d1e824797d96d3738dd3bc9a887d1
Galen Schick <gschick3@gmail.com>
2024-04-07 19:24:15 -0500

    finish assignment requirements

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # payment-system-racket
+
+## How to run
+A default function call is already at the bottom of the program to run with `ACCOUNTS.TXT`, `TRANSACTIONS.TXT`, and `STATEMENTS.TXT`, so simply running the program will get a result.
+
+To customize the input files, the function definition is `generate-report -> String String String Void` and takes arguments in the order listed above (i.e. `(generate-report "accounts.txt" "transactions.txt" "output.txt")`)
+
+## Sources
+### LLMs
+I tried using ChatGPT 3.5 to help with some regex and Typed Racket details, but it was more or less completely useless. I don't believe I actually used anything from the prompts I asked it.
+
+### Other
+I used a lot of StackOverflow, Reddit threads, and, of course, the Racket Documentation.
+
+## Notes
+I tried using Typed Racket for the first time on this project. I learned very quickly that it adds a lot of safey (a.k.a complexity). 
+
+Because of this, a lot of the functions in this project are much larger and messier than I would have made them in untyped Racket.
--- payment-system.rkt
+++ payment-system.rkt
@@ -22,6 +22,7 @@
 (struct Account ([number : Positive-Integer] [name : String] [balance : Flonum]))
 
 (: strings->account (-> String String String (Option Account)))
+; String constructor
 (define (strings->account number name balance)
   (let ([num (string->number number)]
         [bal (string->number balance)])
@@ -32,53 +33,202 @@
 ; Purchase
 (define-type Purchase (List 'purchase Positive-Integer Positive-Integer String Positive-Flonum))
 
-(: mk-purchase (-> Positive-Integer Positive-Integer String Positive-Flonum Purchase))
-(define (mk-purchase account-number timestamp merchant amount)
+(: make-purchase (-> Positive-Integer Positive-Integer String Positive-Flonum Purchase))
+; Basic constructor
+(define (make-purchase account-number timestamp merchant amount)
   (list 'purchase account-number timestamp merchant amount))
 
 (: strings->purchase (-> String String String String (Option Purchase)))
+; String constructor
 (define (strings->purchase account-number timestamp merchant amount)
   (let ([num (string->number account-number)]
         [ts  (string->number timestamp)]
         [amt (string->number amount)])
-    (if (and (exact-positive-integer? num) (exact-positive-integer? ts) (flonum? amt) (positive? amt))
-        (mk-purchase num ts merchant amt)
+    ; requires checks to confirm that types are correct
+    (if (and (exact-positive-integer? num) (exact-positive-integer? ts) (and (flonum? amt) (positive? amt)))
+        (make-purchase num ts merchant amt)
         #f)))
 
 ; Payment
-(: mk-cash-payment (-> Positive-Integer Positive-Integer Positive-Flonum (List 'payment Positive-Integer Positive-Integer 'cash Positive-Flonum)))
-(define (mk-cash-payment account-number timestamp amount)
-  (list 'payment account-number timestamp 'cash amount))
-
-(: mk-check-payment (-> Positive-Integer Positive-Integer Positive-Integer Positive-Flonum (List 'payment Positive-Integer Positive-Integer 'check Positive-Integer Positive-Flonum)))
-(define (mk-check-payment account-number timestamp check-number amount)
-  (list 'payment account-number timestamp 'check check-number amount))
+(define-type Payment (U Cash-Payment Check-Payment Card-Payment))
+(define-type Cash-Payment (List 'payment Positive-Integer Positive-Integer 'cash Positive-Flonum))
+(define-type Check-Payment (List 'payment Positive-Integer Positive-Integer 'check Positive-Integer Positive-Flonum))
+(define-type Card-Payment (List 'payment Positive-Integer Positive-Integer Card-Type Positive-Integer Positive-Flonum))
 
 (define-type Card-Type (U 'credit 'debit))
 
-(: mk-card-payment (-> Positive-Integer Positive-Integer Card-Type Positive-Integer Positive-Flonum (List 'payment Positive-Integer Positive-Integer Card-Type Positive-Integer Positive-Flonum)))
-(define (mk-card-payment account-number timestamp type card-number amount)
+(: make-cash-payment (-> Positive-Integer Positive-Integer Positive-Flonum Cash-Payment))
+; Basic constructor
+(define (make-cash-payment account-number timestamp amount)
+  (list 'payment account-number timestamp 'cash amount))
+
+(: strings->cash-payment (-> String String String (Option Cash-Payment)))
+; String constructor
+(define (strings->cash-payment account-number timestamp amount)
+  (let ([num (string->number account-number)]
+        [ts  (string->number timestamp)]
+        [amt (string->number amount)])
+    (if (and (exact-positive-integer? num) (exact-positive-integer? ts) (and (flonum? amt) (positive? amt)))
+        (make-cash-payment num ts amt)
+        #f)))
+
+(: make-check-payment (-> Positive-Integer Positive-Integer Positive-Integer Positive-Flonum Check-Payment))
+; Basic constructor
+(define (make-check-payment account-number timestamp check-number amount)
+  (list 'payment account-number timestamp 'check check-number amount))
+
+(: strings->check-payment (-> String String String String (Option Check-Payment)))
+; String constructor
+(define (strings->check-payment account-number timestamp check-number amount)
+  (let ([num (string->number account-number)]
+        [ts  (string->number timestamp)]
+        [chk (string->number check-number)]
+        [amt (string->number amount)])
+    (if (and (exact-positive-integer? num) (exact-positive-integer? ts) (exact-positive-integer? chk) (flonum? amt) (positive? amt))
+        (make-check-payment num ts chk amt)
+        #f)))
+
+(: make-card-payment (-> Positive-Integer Positive-Integer Card-Type Positive-Integer Positive-Flonum Card-Payment))
+; Basic constructor
+(define (make-card-payment account-number timestamp type card-number amount)
   (list 'payment account-number timestamp type card-number amount))
 
-(define (strings->transaction [lst : (Listof String)]) : (Option (U Purchase (Listof String)))
-  (cond 
-    [(equal? (first lst) "Purchase") (strings->purchase (second lst) (third lst) (fourth lst) (fifth lst))]
-    [(equal? (first lst) "Payment") (list "payments")]
-    [else (list "none of the above")]))
+(: strings->card-payment (-> String String String String Card-Type (Option Card-Payment)))
+; String constructor
+(define (strings->card-payment account-number timestamp card-number amount type)
+  (let ([num (string->number account-number)]
+        [ts  (string->number timestamp)]
+        [crd (string->number card-number)]
+        [amt (string->number amount)])
+    (if (and (exact-positive-integer? num) (exact-positive-integer? ts) (exact-positive-integer? crd) (flonum? amt) (positive? amt))
+        (make-card-payment num ts type crd amt)
+        #f)))
+
+; Transaction
+(define-type Transaction (U Purchase Payment))
+
+(: strings->transaction (-> (Listof String) (Option Transaction)))
+; String constructor
+(define (strings->transaction lst)
+  (cond
+    [(< (length lst) 5) #f]
+    [(string-ci=? (first lst) "purchase")
+     (strings->purchase (second lst) (third lst) (fourth lst) (fifth lst))]
+    [(string-ci=? (first lst) "payment")
+     (cond
+       [(string-ci=? (fourth lst) "cash")
+        (strings->cash-payment (second lst) (third lst) (fifth lst))]
+       [(and (string-ci=? (fourth lst) "check") (>= (length lst) 6))
+        (strings->check-payment (second lst) (third lst) (fifth lst) (sixth lst))]
+       [(string-ci=? (fourth lst) "credit") (strings->card-payment (second lst) (third lst) (fifth lst) (sixth lst) 'credit)]
+       [(string-ci=? (fourth lst) "debit") (strings->card-payment (second lst) (third lst) (fifth lst) (sixth lst) 'debit)]
+       [else #f])]
+    [else #f]))
+
 
 ; Reading files
 
 ; Split line on all spaces while keeping quoted strings together
-(define (split-line [line : String]) : (Listof String)
+(: split-line (-> String (Listof String)))
+(define (split-line line)
   (regexp-match* (pregexp "(?<=\")[^\\s][^\"]+[^\\s](?=\")|[\\w.]+") line))
 
-(define (read-accounts [filename : String]) : (Listof Account)
+; Read account information from file
+(: read-accounts (-> String (Listof Account)))
+(define (read-accounts filename)
   (filter-options (map (lambda ([l : String])
                          (let ([line : (Listof String) (split-line l)])
                            (strings->account (first line) (second line) (third line))))
                        (file->lines filename))))
 
-(define (read-transactions [filename : String]) : (Listof (U Purchase (Listof String)))
+; Read transaction information from file
+(: read-transactions (-> String (Listof Transaction)))
+(define (read-transactions filename)
   (filter-options (map (lambda ([l : String])
                          (strings->transaction (split-line l)))
                        (file->lines filename))))
+
+
+; Processing transactions
+
+; Get all transactions for given account
+(: get-account-transactions (-> Account (Listof Transaction) (Listof Transaction)))
+(define (get-account-transactions account transactions)
+  (filter (lambda ([transaction : Transaction])
+            (equal? (Account-number account) (second transaction)))
+          transactions))
+
+; Fold all transactions to total purchases and payments
+(: total-transactions (-> (Listof Transaction) (List Flonum Flonum)))
+(define (total-transactions transactions)
+  (foldl (lambda ([transaction : Transaction] [stats : (List Flonum Flonum)])
+           (let ([amt (last transaction)])
+             (cond
+               [(not (flonum? amt)) stats] ; Type checker needs confirmation
+               [(equal? (first transaction) 'purchase) (list (+ (first stats) amt) (second stats))]
+               [else (list (first stats) (+ (second stats) amt))])))
+         '(0.0 0.0)
+         transactions))
+
+
+; Output
+(define col-width 10)
+
+; Write everything to file
+(: write-all (-> (Listof Account) (Listof Transaction) String Void))
+(define (write-all accounts transactions path)
+  (display-to-file (all->string accounts transactions) path #:exists 'replace))
+
+; Turn everything into a string
+(: all->string (-> (Listof Account) (Listof Transaction) String))
+(define (all->string accounts transactions)
+  (string-join (map (lambda ([account : Account])
+                      (let ([account-transactions (get-account-transactions account transactions)])
+                        (format "STATEMENT OF ACCOUNT\n~a\n\n~a\n\n~a\n\n~a"
+                                (account->string account)
+                                (string-join (map transaction->string account-transactions) "\n")
+                                (summary->string account account-transactions)
+                                (~a #:width 50 #:pad-string "*"))))
+                    accounts)
+               "\n"))
+
+; Get string of account header
+(: account->string (-> Account String))
+(define (account->string account)
+  (~a (~a (Account-number account) #:min-width 20)
+      (~a (Account-name account) #:min-width 20)
+      (~a "Starting Balance: " (~r (Account-balance account) #:precision `(= 2)) #:min-width 20)))
+
+; Get string of transaction
+(: transaction->string (-> Transaction String))
+(define (transaction->string transaction)
+  (cond
+    [(equal? (first transaction) 'purchase)
+     (~a (~a (third transaction) #:min-width col-width)
+         (~a "Purchase" #:min-width col-width)
+         (~a (fourth transaction) #:min-width 20 #:max-width 40)
+         (~r (fifth transaction) #:min-width col-width #:precision '(= 2)))]
+    [else
+     (~a (~a (third transaction) #:min-width col-width)
+         (~a "Payment" #:min-width col-width)
+         (string-titlecase (~a (fourth transaction) #:min-width 20))
+         (~r (let ([amt (last transaction)]) (if (flonum? amt) amt 0.0)) #:min-width col-width #:precision '(= 2)))]))
+
+; Get string of account summary
+(: summary->string (-> Account (Listof Transaction) String))
+(define (summary->string account transactions)
+  (let ([summary (total-transactions transactions)])
+    (format "Total Purchases:~a\nTotal Payments: ~a\nEnding Balance: ~a"
+            (~r (first summary) #:min-width col-width #:precision '(= 2))
+            (~r (second summary) #:min-width col-width #:precision '(= 2))
+            (~r (+ (Account-balance account) (- (first summary) (second summary))) #:min-width col-width #:precision '(= 2)))))
+
+
+; Driver
+(: generate-report (-> String String String Void))
+(define (generate-report accounts-path transactions-path output-path)
+  (write-all (read-accounts accounts-path) (read-transactions transactions-path) output-path))
+
+
+
+(generate-report "ACCOUNTS.TXT" "TRANSACTIONS.TXT" "STATEMENTS.TXT")
